d784212afdda8d16d293799bf8cf81e15d7432e0
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2024-11-01 23:13:17 +0100

    TODO: Vytvořit návrh pro generování 3AK a sémantickou analýzu
    Nahrané soubory obsahují nějaké vygenerované návrhy prototypů pro
    starou verzi AST návrhu (tedy nejsou kompatibilní se současnou).
    Sémantická analýza je čistě implementační záležitost, tudíž autorovi
    stačí vytvářet návrh "za běhu". U 3AK by bylo dobré vytvořit hrubý
    nástřel návrhu s dokumentací s předstihem.
    
    PS: Je vhodné, aby autor sémantické analýzy v autorem 3AK generátoru
        domluven, jak bude například modifikovat parserem vygenerovaný
        AST při nutnosti přetypování, volání vestavěných funkcí apod.

---
+++ src/tac_generator.c
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *                                                                             *
+ * Název projektu:   Implementace překladače imperativního jazyka IFJ24        *
+ *                                                                             *
+ * Soubor:           tac_generator.c                                           *
+ * Autor:            XXX XXXXXX   <xplagia00>                                  *
+ *                                                                             *
+ * Datum:            XX.XX.2024                                                *
+ * Poslední změna:   XX.XX.2024                                                *
+ *                                                                             *
+ * Tým:      Tým xkalinj00                                                     *
+ * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
+ *           Hýža Pavel           <xhyzapa00>                                  *
+ *           Kalina Jan           <xkalinj00>                                  *
+ *           Krejčí David         <xkrejcd00>                                  *
+ *                                                                             *
+ ******************************************************************************/
+/**
+ * @file tac_generator.c
+ * @author XXX XXXXX \<xplagia00>
+ *
+ * @brief Implementace funkcí pro generátor vnitřního kódu (3AK).
+ * @details XXX
+ */
+
+
+
+
+
+
+/*** Konec souboru tac_generator.c ***/
