275e1b450a8067a28926b48231eb2418862f6b84
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2024-11-06 18:05:42 +0100

    Vytvořeny zdojové soubory s funkcí miain() překladače
    V rámci implementace parseru bude nutné začít vytvářet také funkci
    main celého překladače, která bude řídit průběh překladu, resp.
    bude určovat, kdy se má přejít z fáze parsování do fáze sémantické
    analýzy, generování 3AK a výsledného assembleru.

---
+++ src/ifj24_compiler.c
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *                                                                             *
+ * Název projektu:   Implementace překladače imperativního jazyka IFJ24        *
+ *                                                                             *
+ * Soubor:           ifj24_compiler.c                                          *
+ * Autor:            Jan Kalina   <xkalinj00>                                  *
+ *                                                                             *
+ * Datum:            06.11.2024                                                *
+ * Poslední změna:   06.11.2024                                                *
+ *                                                                             *
+ * Tým:      Tým xkalinj00                                                     *
+ * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
+ *           Hýža Pavel           <xhyzapa00>                                  *
+ *           Kalina Jan           <xkalinj00>                                  *
+ *           Krejčí David         <xkrejcd00>                                  *
+ *                                                                             *
+ ******************************************************************************/
+/**
+ * @file ifj24_compiler.c
+ * @author Jan Kalina \<xkalinj00>
+ *
+ * @brief Implementační soubor pro funkci `main()` překladače jazyka IFJ24.
+ * @details Tento soubor obsahuje implementaci funkce `main()`, která slouží
+ *          jako vstupní bod programu a zajišťuje spuštění všech potřebných
+ *          submodulů překladače pro jazyk IFJ24.
+ */
+
+
+#include "ifj24_compiler.h"
+
+int main(int argc, char *argv[]) {
+
+
+    return SUCCESS;
+}
---
+++ src/ifj24_compiler.h
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ *                                                                             *
+ * Název projektu:   Implementace překladače imperativního jazyka IFJ24        *
+ *                                                                             *
+ * Soubor:           ifj24_compiler.h                                          *
+ * Autor:            Jan Kalina   <xkalinj00>                                  *
+ *                                                                             *
+ * Datum:            06.11.2024                                                *
+ * Poslední změna:   06.11.2024                                                *
+ *                                                                             *
+ * Tým:      Tým xkalinj00                                                     *
+ * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
+ *           Hýža Pavel           <xhyzapa00>                                  *
+ *           Kalina Jan           <xkalinj00>                                  *
+ *           Krejčí David         <xkrejcd00>                                  *
+ *                                                                             *
+ ******************************************************************************/
+/**
+ * @file ifj24_compiler.h
+ * @author Jan Kalina \<xkalinj00>
+ *
+ * @brief Hlavičkový soubor pro funkci `main()` překladače jazyka IFJ24.
+ * @details Tento soubor obsahuje pouze deklaraci funkce main, která slouží jako
+ *          vstupní bod programu a zajišťuje spuštění všech potřebných submodulů
+ *          překladače pro jazyk IFJ24.
+ */
+
+#ifndef MAIN_H_
+/** @cond  */
+#define MAIN_H_
+/** @endcond  */
+
+
+#include "error.h"
+#include "parser.h"
+#include "semantic_analyser.h"
+#include "tac_generator.h"
+
+/**
+ * @brief Konstanta označující úspěšné dokončení překladu
+ */
+#define SUCCESS 0
+
+/*******************************************************************************
+ *                                                                             *
+ *                   FUNKCE "MAIN()" PŘEKLADAČE JAZYKA IFJ24                   *
+ *                                                                             *
+ ******************************************************************************/
+
+/**
+ * @brief Hlavní funkce programu.
+ * @details Funkce `main` zpracovává argumenty příkazové řádky, spouští
+ *          inicializaci, překlad a uvolnění prostředků překladače pomocí
+ *          volání příslušných funkcí jednotlivých submodulů překladače
+ *
+ * @param argc Počet argumentů příkazové řádky.
+ * @param argv Pole řetězců s argumenty příkazové řádky.
+ *
+ * @return @c SUCCESS při úspěchu, jinak @c ErrorType hodnota při chybě.
+ */
+int main(int argc, char *argv[]);
+
+#endif  // MAIN_H_
