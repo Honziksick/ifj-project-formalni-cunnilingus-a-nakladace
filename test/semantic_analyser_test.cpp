b72c339249ef3ae1a512c5839c731089f3c76b8c
David Krejčí <xkrejcd00@stud.fit.vutbr.cz>
2024-11-17 17:55:37 +0100

    Přidány základní testy pro sémantický analyzátor

--- test/semantic_analyser_test.cpp
+++ test/semantic_analyser_test.cpp
@@ -2,11 +2,11 @@
  *                                                                             *
  * Název projektu:   Implementace překladače imperativního jazyka IFJ24        *
  *                                                                             *
- * Soubor:           ast_test.cpp                                              *
- * Autor:            Jan Kalina   <xkalinj00>                                  *
+ * Soubor:           semantic_analyser.cpp                                     *
+ * Autor:            Krejčí David   <xkrejcd00>                                *
  *                                                                             *
- * Datum:            08.11.2024                                                *
- * Poslední změna:   08.11.2024                                                *
+ * Datum:            17.11.2024                                                *
+ * Poslední změna:   17.11.2024                                                *
  *                                                                             *
  * Tým:      Tým xkalinj00                                                     *
  * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
@@ -16,19 +16,23 @@
  *                                                                             *
  ******************************************************************************/
 /**
- * @file ast_test.cpp
- * @author Jan Kalina \<xkalinj00>
+ * @file semantic_analyser.cpp
+ * @author Krejčí David \<xkrejcd00>
  *
- * @brief Testy stavby abstraktního syntaktického stromu AST.
+ * @brief Testy sémantické analýzy
  * @details Tento zdrojový soubor obsahuje unit testy funkcí implementovaných
- *          v souboru `ast_interface.c`. Testy jsou vytvořené za využití 
- *          frameworku Google Test.
+ *          v souboru `semantic_analyser.c`. Testy jsou vytvořené za využití 
+ *          frameworku Google Test. Testy vychází z funkce lexkíláního
+ *          analyzátoru a testují nejen sémantickou analýzu, ale i vytváření
+ *          abstraktního syntaktického stromu.
  */
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
 extern "C" {
+#include "parser.h"
+#include "llparser.h"
 #include "semantic_analyser.h"
 }
 
@@ -40,3 +44,114 @@ using namespace internal;
  * @brief Globální kořen abstraktního syntaktického stromu.
  */
 AST_ProgramNode *ASTroot;
+
+void TestSemantic(){
+    semantic_analyseProgram();
+    exit(0);
+}
+
+TEST(Correct, Simplest){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/semantic_examples/simplest.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+    ASTroot = NULL;
+}
+
+TEST(Correct, Hello){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/hello.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(Correct, Example1){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/example1.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(Correct, Example2){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/example2.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(Correct, Example3){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/example3.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(Correct, Fun){
+    FILE* f = fopen("../ifj24_examples/ifj24_programs/fun.zig", "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+
+    frameStack_init();
+
+    ASTroot = LLparser_parseProgram();
+    ASSERT_NE(ASTroot, nullptr);
+    EXPECT_EXIT(TestSemantic(), ExitedWithCode(0), "");
+
+    frameStack_destroyAll();
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+
+
