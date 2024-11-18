f7a3ddac329207e3f3c31a6fa96514b86dd3e953
RyzaGrey <pavelhyza666@email.cz>
2024-11-18 17:08:14 +0100

    Přidání testu pro string v Lex Testy escape sekvencí ve stringu.

---
+++ test/test_examples/lexical_examples/lex_test_string_escapes.zig
@@ -0,0 +1,4 @@
+ifj.concat(
+    "Toto je test escape sekvenci stringu. Lomeno n: \n; Lomeno lomeno: \\; Lomeno uvozovky: \"; Lomeno r: \r; Lomeno t: \t.",
+    "Toto je test x escape sekvence stringu. :(58) = \x3A; }(125) = \x7D; NL(10) = \x0A; ú(250) = \xFA."
+);
