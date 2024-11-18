c30f9bc2a3c657b095a686c1e34c87b0007fbe75
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2024-11-18 16:42:54 +0100

    Oprava skriptu ifj24_examples/examples.sh
    Soubor chybně bral celou cestu k souborům namísto pouze jejich názvu.

--- ifj24_examples/examples.sh
+++ ifj24_examples/examples.sh
@@ -1,10 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-# IFJ24 - Priklady pro studenty pro dokresleni zadani (spusteni konfigurovano pro server Merlin)
+# IFJ24 - Příklady pro studenty pro dokreslení zadání (spuštění konfigurováno pro server Merlin)
 
-COMPILER=""  # zde muze byt vas prekladac IFJ24 (napr. ./ifj24)
+COMPILER=""  # zde může být váš překladač IFJ24 (např. ./ifj24)
 HELPER="ifj24.zig"
-INTERPRETER="./run.sh"  # zde muze byt interpret IFJcode24 (./ic24int)
+INTERPRETER="./run.sh"  # zde může být interpret IFJcode24 (např. ./ic24int)
 
 for i in ./ifj24_programs/*.zig; do
 
@@ -12,25 +12,23 @@ for i in ./ifj24_programs/*.zig; do
         continue
     fi
 
-    IN=`echo $i | sed 's/ifj24_programs/in_reference/' | sed 's/\(.*\)\.zig/\1.in/g'`
-    OUT=`echo $i | sed 's/ifj24_programs/out_reference/' | sed 's/\(.*\)\.zig/\1.out/g'`
-    PRG=$i
+    IN=$(echo "$i" | sed 's#./ifj24_programs/#in_reference/#' | sed 's/\(.*\)\.zig/\1.in/')
+    OUT=$(echo "$i" | sed 's#./ifj24_programs/#out_reference/#' | sed 's/\(.*\)\.zig/\1.out/')
+    PRG=$(basename "$i")
 
-    # $COMPILER $i $HELPER > $PRG   # nekontroluji se chyby prekladu (pro chybove testy nutno rozsirit)
+    # $COMPILER $i $HELPER > $PRG   # nekontrolují se chyby překladu (pro chybové testy nutno rozšířit)
     # RETURNED_COMPILER=$?
 
     echo -n "DIFF: $INTERPRETER $PRG and $OUT: "
-    cat $IN | $INTERPRETER $PRG 2> >(sed $'s,.*,\e[31m&\e[m,'>&2) | diff - $OUT > /dev/null 2> /dev/null
+    cat "$IN" | $INTERPRETER "$PRG" 2> >(sed $'s,.*,\e[31m&\e[m,' >&2) | diff - "$OUT" > /dev/null 2> /dev/null
     DIFFOK=$?
-    if [ "x$DIFFOK" == "x0" ]; then
+    if [ "$DIFFOK" -eq 0 ]; then
         echo "OK"
     else
         echo "DIFFERENCE to $OUT"
-        cat $IN | $INTERPRETER $PRG 2> >(sed $'s,.*,\e[31m&\e[m,'>&2) | diff - $OUT 
+        cat "$IN" | $INTERPRETER "$PRG" 2> >(sed $'s,.*,\e[31m&\e[m,' >&2) | diff - "$OUT"
     fi
     echo
-done;
-
-# rm *.tmp
+done
 
 exit 0
