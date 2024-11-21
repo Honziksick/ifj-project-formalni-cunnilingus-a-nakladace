dc5770b7b0065ad862aedd59672eb87db8ca92b3
David Krejčí <xkrejcd00@stud.fit.vutbr.cz>
2024-11-21 18:25:20 +0100

    Update na ifj24_examples složku

--- ifj24_examples/examples.sh
+++ ifj24_examples/examples.sh
@@ -1,34 +1,36 @@
-#!/bin/bash
+#/bin/bash
 
-# IFJ24 - Příklady pro studenty pro dokreslení zadání (spuštění konfigurováno pro server Merlin)
+# IFJ24 - Priklady pro studenty pro dokresleni zadani (spusteni konfigurovano pro server Merlin)
 
-COMPILER=""  # zde může být váš překladač IFJ24 (např. ./ifj24)
+COMPILER=""  # zde muze byt vas prekladac IFJ24 (napr. ./ifj24)
 HELPER="ifj24.zig"
-INTERPRETER="./run.sh"  # zde může být interpret IFJcode24 (např. ./ic24int)
+INTERPRETER="./run.sh"  # zde muze byt interpret IFJcode24 (./ic24int)
 
-for i in ./ifj24_programs/*.zig; do
+for i in *.zig; do
 
-    if [[ $i == "./ifj24_programs/$HELPER" ]]; then
-        continue
-    fi
+	if [[ $i == $HELPER ]]; then
+		continue
+	fi
 
-    IN=$(echo "$i" | sed 's#./ifj24_programs/#in_reference/#' | sed 's/\(.*\)\.zig/\1.in/')
-    OUT=$(echo "$i" | sed 's#./ifj24_programs/#out_reference/#' | sed 's/\(.*\)\.zig/\1.out/')
-    PRG=$(basename "$i")
+	IN=`echo $i | sed 's/\(.*\)\.zig/\1.in/g'`
+	OUT=`echo $i | sed 's/\(.*\)\.zig/\1.out/g'`
+	PRG=$i
 
-    # $COMPILER $i $HELPER > $PRG   # nekontrolují se chyby překladu (pro chybové testy nutno rozšířit)
-    # RETURNED_COMPILER=$?
+	# $COMPILER $i $HELPER > $PRG   # nekontroluji se chyby prekladu (pro chybove testy nutno rozsirit)
+	# RETURNED_COMPILER=$?
 
-    echo -n "DIFF: $INTERPRETER $PRG and $OUT: "
-    cat "$IN" | $INTERPRETER "$PRG" 2> >(sed $'s,.*,\e[31m&\e[m,' >&2) | diff - "$OUT" > /dev/null 2> /dev/null
-    DIFFOK=$?
-    if [ "$DIFFOK" -eq 0 ]; then
-        echo "OK"
-    else
-        echo "DIFFERENCE to $OUT"
-        cat "$IN" | $INTERPRETER "$PRG" 2> >(sed $'s,.*,\e[31m&\e[m,' >&2) | diff - "$OUT"
-    fi
-    echo
-done
+	echo -n "DIFF: $INTERPRETER $PRG and $OUT: "
+	cat $IN | $INTERPRETER $PRG 2> >(sed $'s,.*,\e[31m&\e[m,'>&2) | diff - $OUT > /dev/null 2> /dev/null
+	DIFFOK=$?
+	if [ "x$DIFFOK" == "x0" ]; then
+		echo "OK"
+	else
+		echo "DIFFERENCE to $OUT"
+		cat $IN | $INTERPRETER $PRG 2> >(sed $'s,.*,\e[31m&\e[m,'>&2) | diff - $OUT 
+	fi
+	echo
+done;
+
+# rm *.tmp
 
 exit 0
