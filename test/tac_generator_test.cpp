d82147f5b2948cb230a523f59d76fe136afb8303
NormREE <xfarkal00@stud.fit.vutbr.cz>
2024-11-26 19:28:35 +0100

    Testy pro nové generování kódu

--- test/tac_generator_test.cpp
+++ test/tac_generator_test.cpp
@@ -38,12 +38,8 @@ using namespace std;
 using namespace testing;
 using namespace internal;
 
-TEST(TAC, Print_Test_Code) {
-    TAC_generateTestCode();
-}
-
-TEST(TAC, Generate_Program_Code) {
-    string path = synt_path + "correct_two_fun.zig";
+TEST(TAC, generate_example1_statement){
+    string path = exam_path + "example1.zig";
     FILE* f = fopen(path.c_str(), "r");
     ASSERT_NE(f, nullptr);
     FILE* stdin_backup = stdin;
@@ -58,52 +54,10 @@ TEST(TAC, Generate_Program_Code) {
     // Kořen je inicializován
     EXPECT_NE(ASTroot, nullptr);
 
-    // Má funkci
-    EXPECT_NE(ASTroot->functionList, nullptr);
-    AST_FunDefNode* fun = ASTroot->functionList;
-    // Funkce nemá tělo
-    EXPECT_EQ(fun->body, nullptr);
-    // Název funkce je main
-    char *comp = string_toConstChar(fun->identifier);
-    EXPECT_STREQ(comp, "main");
-    free(comp);
-    // Vrací void
-    EXPECT_EQ(fun->returnType, AST_DATA_TYPE_VOID);
-    // Nemá parametry
-    EXPECT_EQ(fun->parameters, nullptr);
-    // Má ukazatel na další funkci
-    EXPECT_NE(fun->next, nullptr);
+    AST_FunDefNode *nodeFun = ASTroot->functionList;
+    AST_StatementNode *nodeStat = nodeFun->body;
 
-    fun = fun->next;
-    // Druhá funkce
-    // Funkce nemá tělo
-    EXPECT_EQ(fun->body, nullptr);
-    // Má identifikátor
-    EXPECT_NE(fun->identifier, nullptr);
-    // Vrací void
-    EXPECT_EQ(fun->returnType, AST_DATA_TYPE_VOID);
-    // Nemá parametry
-    EXPECT_EQ(fun->parameters, nullptr);
-    // Nemá na další funkci
-    EXPECT_EQ(fun->next, nullptr);
-    
-    // Tisk obdrženého stromu a stavu zásobníku rámcu pro vizuální kontrolu
-    PRINT_TEST_LOG(CorrectTwoFunctions);
-
-    TAC_generateProgramCodeBegin(ASTroot);
-
-    // Projdeme všechny funkce a vygenerujeme kód pro každou z nich
-    AST_FunDefNode *current = ASTroot->functionList;
-    while(current != NULL){
-
-        TAC_generateFunctionDefinitionBegin(current);
-
-        current = current->next;
-
-        TAC_generateFunctionDefinitionEnd();
-    }
-
-    TAC_generateProgramCodeEnd();
+    TAC_generateStatementBlock(nodeStat);
 
     // Uvolnění alokovaných zdrojů
     IFJ24Compiler_freeAllAllocatedMemory();
@@ -113,13 +67,13 @@ TEST(TAC, Generate_Program_Code) {
     fclose(f);
 }
 
-TEST(TAC, Generate_Operator){
-    string path = synt_path + "X_correct_rel_operators_LE.zig";
+TEST(TAC, generate_example1_expression){
+    string path = exam_path + "example1.zig";
     FILE* f = fopen(path.c_str(), "r");
     ASSERT_NE(f, nullptr);
     FILE* stdin_backup = stdin;
     stdin = f;
-
+    
     // Inicializace zásobníku rámců
     frameStack_init();
 
@@ -129,18 +83,66 @@ TEST(TAC, Generate_Operator){
     // Kořen je inicializován
     EXPECT_NE(ASTroot, nullptr);
 
-    // Tisk obdrženého stromu a stavu zásobníku rámcu pro vizuální kontrolu
-    PRINT_TEST_LOG(CorrectRelOperatorsLE);
+    AST_FunDefNode *nodeFun = ASTroot->functionList;
+    AST_StatementNode *nodeStat = nodeFun->body;
+    AST_StatementNode *statement = (AST_StatementNode *)(nodeStat->statement);
+    AST_ExprNode *nodeExpr = (AST_ExprNode *)(statement->statement);
 
-    AST_FunDefNode *func = ASTroot->functionList;
+    TAC_generateExpression(nodeExpr);
 
-    AST_ExprNode *expr = (AST_ExprNode*)(func->body->statement);
-    TAC_generateExpression(expr);
+    // Uvolnění alokovaných zdrojů
+    IFJ24Compiler_freeAllAllocatedMemory();
 
-    AST_StatementNode *nextStmt = (AST_StatementNode *)((AST_StatementNode *)func->body->statement)->next;
-    AST_IfNode *ifNode = (AST_IfNode *)nextStmt->statement;
-    AST_ExprNode *expr2 = ifNode->condition;
-    TAC_generateExpression(expr2);
+    // Navrácení STDIN do původního stavu a uzavření souboru
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(TAC, generate_example1_funDef){
+    string path = exam_path + "example1.zig";
+    FILE* f = fopen(path.c_str(), "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+    
+    // Inicializace zásobníku rámců
+    frameStack_init();
+
+    // Syntaktická analýza programu
+    LLparser_parseProgram();
+
+    // Kořen je inicializován
+    EXPECT_NE(ASTroot, nullptr);
+
+    AST_FunDefNode *nodeFun = ASTroot->functionList;
+
+    TAC_generateFunctionDefinition(nodeFun);
+
+    // Uvolnění alokovaných zdrojů
+    IFJ24Compiler_freeAllAllocatedMemory();
+
+    // Navrácení STDIN do původního stavu a uzavření souboru
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(TAC, generate_example1){
+    string path = exam_path + "example1.zig";
+    FILE* f = fopen(path.c_str(), "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+    
+    // Inicializace zásobníku rámců
+    frameStack_init();
+
+    // Syntaktická analýza programu
+    LLparser_parseProgram();
+
+    // Kořen je inicializován
+    EXPECT_NE(ASTroot, nullptr);
+
+    TAC_generateProgram();
 
     // Uvolnění alokovaných zdrojů
     IFJ24Compiler_freeAllAllocatedMemory();
