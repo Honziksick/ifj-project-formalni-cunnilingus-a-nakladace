228509ac35e566b45c35bbf922942905e80a3941
xfarkal00 <xfarkal00@stud.fit.vutbr.cz>
2024-11-26 21:07:20 +0100

    testy++

--- test/tac_generator_test.cpp
+++ test/tac_generator_test.cpp
@@ -60,6 +60,7 @@ TEST(TAC, generate_example1_statement){
     EXPECT_NE(ASTroot, nullptr);
 
     AST_FunDefNode *nodeFun = ASTroot->functionList;
+
     AST_StatementNode *nodeStat = nodeFun->body;
     AST_FunCallNode *funCall = (AST_FunCallNode*)nodeStat->statement;
     AST_ExprNode *thisis = funCall->arguments->expression;
@@ -68,37 +69,6 @@ TEST(TAC, generate_example1_statement){
     PRINT_AST(AST_PROGRAM_NODE, ASTroot);
 
     TAC_generateStatementBlock(nodeStat);
-
-    // Uvolnění alokovaných zdrojů
-    IFJ24Compiler_freeAllAllocatedMemory();
-
-    // Navrácení STDIN do původního stavu a uzavření souboru
-    stdin = stdin_backup;
-    fclose(f);
-}
-
-TEST(TAC, generate_example1_expression){
-    string path = exam_path + "example1.zig";
-    FILE* f = fopen(path.c_str(), "r");
-    ASSERT_NE(f, nullptr);
-    FILE* stdin_backup = stdin;
-    stdin = f;
-    
-    // Inicializace zásobníku rámců
-    frameStack_init();
-
-    // Syntaktická analýza programu
-    LLparser_parseProgram();
-
-    // Kořen je inicializován
-    EXPECT_NE(ASTroot, nullptr);
-
-    AST_FunDefNode *nodeFun = ASTroot->functionList;
-    AST_StatementNode *nodeStat = nodeFun->body;
-    AST_StatementNode *statement = (AST_StatementNode *)(nodeStat->statement);
-    AST_ExprNode *nodeExpr = (AST_ExprNode *)(statement->statement);
-
-    TAC_generateExpression(nodeExpr);
 
     // Uvolnění alokovaných zdrojů
     IFJ24Compiler_freeAllAllocatedMemory();
@@ -136,8 +106,121 @@ TEST(TAC, generate_example1_funDef){
     fclose(f);
 }
 
+TEST(TAC, generate_example1_binary){
+    string path = exam_path + "example1.zig";
+    FILE* f = fopen(path.c_str(), "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+    
+    // Inicializace zásobníku rámců
+    frameStack_init();
+
+    // Syntaktická analýza programu
+    LLparser_parseProgram();
+
+    // Kořen je inicializován
+    EXPECT_NE(ASTroot, nullptr);
+
+    // Ukazatel na funkci main
+    AST_FunDefNode *nodeFun = ASTroot->functionList;
+
+    // Ukazatel na tělo funkce main --> volání funkce
+    AST_StatementNode *nodeStat = nodeFun->body;
+    EXPECT_EQ(nodeStat->statementType, AST_STATEMENT_FUN_CALL);
+
+    // Ukazatel na další příkaz v těle funkce main --> definice proměnné
+    AST_StatementNode *statement = (AST_StatementNode *)(nodeStat->next);
+    EXPECT_EQ(statement->statementType, AST_STATEMENT_VAR_DEF);
+
+    // Ukazatel na výraz v definici proměnné
+    AST_ExprNode *nodeExpr = (AST_ExprNode *)(statement->statement);
+    EXPECT_EQ(nodeExpr->exprType, AST_EXPR_BINARY_OP);
+
+    TAC_generateBinaryOperator((AST_BinOpNode *)nodeExpr->expression);
+
+    // Uvolnění alokovaných zdrojů
+    IFJ24Compiler_freeAllAllocatedMemory();
+
+    // Navrácení STDIN do původního stavu a uzavření souboru
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+TEST(TAC, generate_example1_if){
+    string path = exam_path + "example1.zig";
+    FILE* f = fopen(path.c_str(), "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+    
+    // Inicializace zásobníku rámců
+    frameStack_init();
+
+    // Syntaktická analýza programu
+    LLparser_parseProgram();
+
+    // Kořen je inicializován
+    EXPECT_NE(ASTroot, nullptr);
+
+    // Ukazatel na funkci main
+    AST_FunDefNode *nodeFun = ASTroot->functionList;
+
+    // Ukazatel na tělo funkce main --> volání funkce
+    AST_StatementNode *nodeStat = nodeFun->body;
+    EXPECT_EQ(nodeStat->statementType, AST_STATEMENT_FUN_CALL);
+
+    // Ukazatel na další příkaz v těle funkce main --> definice proměnné
+    AST_StatementNode *statement = (AST_StatementNode *)(nodeStat->next);
+    EXPECT_EQ(statement->statementType, AST_STATEMENT_VAR_DEF);
+
+    // Další statement v těle funkce main --> if
+    statement = (AST_StatementNode *)(statement->next);
+    EXPECT_EQ(statement->statementType, AST_STATEMENT_IF);
+
+    AST_IfNode *ifNode = (AST_IfNode *)statement->statement;
+    EXPECT_EQ(ifNode->type, AST_IF_NODE);
+    EXPECT_NE(ifNode->condition, nullptr);
+
+    TAC_generateIf(ifNode);
+
+    // Uvolnění alokovaných zdrojů
+    IFJ24Compiler_freeAllAllocatedMemory();
+
+    // Navrácení STDIN do původního stavu a uzavření souboru
+    stdin = stdin_backup;
+    fclose(f);
+}
+
 TEST(TAC, generate_example1){
     string path = exam_path + "example1.zig";
+    FILE* f = fopen(path.c_str(), "r");
+    ASSERT_NE(f, nullptr);
+    FILE* stdin_backup = stdin;
+    stdin = f;
+    
+    // Inicializace zásobníku rámců
+    frameStack_init();
+
+    // Syntaktická analýza programu
+    LLparser_parseProgram();
+
+    // Kořen je inicializován
+    EXPECT_NE(ASTroot, nullptr);
+
+    TAC_generateProgram();
+
+    // Uvolnění alokovaných zdrojů
+    IFJ24Compiler_freeAllAllocatedMemory();
+
+    // Navrácení STDIN do původního stavu a uzavření souboru
+    stdin = stdin_backup;
+    fclose(f);
+}
+
+
+TEST(TAC, generate_example2){
+    string path = exam_path + "example2.zig";
     FILE* f = fopen(path.c_str(), "r");
     ASSERT_NE(f, nullptr);
     FILE* stdin_backup = stdin;
